aee05751f4ed6a4ea2afbce93f6a05dcdbd901dc
Piotr Karpala <pkarpala@infusion.com>
2015-09-12 18:14:12 +0200

    solution with tests

--- KattisSolution/Program.cs
+++ KattisSolution/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Numerics;
 using KattisSolution.IO;
 
 namespace KattisSolution
@@ -15,15 +16,87 @@ namespace KattisSolution
         {
             IScanner scanner = new OptimizedPositiveIntReader(stdin);
             // uncomment when you need more advanced reader
-            // scanner = new Scanner(stdin);
+            scanner = new Scanner(stdin);
             // scanner = new LineReader(stdin);
-            BufferedStdoutWriter writer = new BufferedStdoutWriter(stdout);
+            var writer = new BufferedStdoutWriter(stdout);
 
-            var input = scanner.NextInt();
+            var numberOfTestCases = scanner.NextInt();
 
-            writer.Write(input * 5);
-            writer.Write("\n");
+            for (var i = 0; i < numberOfTestCases; i++)
+            {
+                BigInteger x1 = scanner.NextInt();
+                BigInteger y1 = scanner.NextInt();
+                var operation = scanner.Next();
+                BigInteger x2 = scanner.NextInt();
+                BigInteger y2 = scanner.NextInt();
+
+                Tuple<BigInteger, BigInteger> result;
+                switch (operation)
+                {
+                    case "+":
+                        result = Add(x1, y1, x2, y2);
+                        break;
+                    case "-":
+                        // to substract is like to add but with the oposite sign
+                        result = Add(x1, y1, -x2, y2);
+                        break;
+                    case "*":
+                        result = Multiply(x1, y1, x2, y2);
+                        break;
+                    case "/":
+                        // to divide is like to multiple by the oposite
+                        result = Multiply(x1, y1, y2, x2);
+                        break;
+                    default:
+                        throw new InvalidOperationException("no operation for:" + operation);
+                }
+                writer.Write(result.Item1);
+                writer.Write(" / ");
+                writer.Write(result.Item2);
+                writer.Write("\n");
+            }
+
             writer.Flush();
+        }
+
+        private static Tuple<BigInteger, BigInteger> Multiply(BigInteger x1, BigInteger y1, BigInteger x2, BigInteger y2)
+        {
+            var fractionBase = y1 * y2;
+            var fractionTop = x1 * x2;
+
+            return Simplify(fractionTop, fractionBase);
+        }
+
+        private static Tuple<BigInteger, BigInteger> Add(BigInteger x1, BigInteger y1, BigInteger x2, BigInteger y2)
+        {
+            BigInteger fractionBase, fractionTop;
+
+            if (y1 == y2)
+            {
+                fractionTop = x1 + x2;
+                fractionBase = y1;
+            }
+            else
+            {
+                fractionTop = x1 * y2 + x2 * y1;
+                fractionBase = y1 * y2;
+            }
+
+            return Simplify(fractionTop, fractionBase);
+        }
+
+        private static Tuple<BigInteger, BigInteger> Simplify(BigInteger x, BigInteger y)
+        {
+            BigInteger i = BigInteger.GreatestCommonDivisor(x, y); ;
+
+            while (i != 1)
+            {
+                x /= i;
+                y /= i;
+                i = BigInteger.GreatestCommonDivisor(x, y);
+            }
+
+            return new Tuple<BigInteger, BigInteger>(x, y);
         }
     }
 }
--- KattisSolution/Tests/CustomTest.cs
+++ KattisSolution/Tests/CustomTest.cs
@@ -4,7 +4,6 @@ using NUnit.Framework;
 
 namespace KattisSolution.Tests
 {
-    [Ignore]
     [TestFixture]
     [Category("sample")]
     public class CustomTest
@@ -13,8 +12,127 @@ namespace KattisSolution.Tests
         public void SampleTest_WithStringData_Should_Pass()
         {
             // Arrange
-            const string expectedAnswer = "50\n";
-            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("10\n")))
+            const string expectedAnswer = "500000001 / 50000000\n";
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("1\n100000001 100000000 + 900000001 100000000")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass2()
+        {
+            // Arrange
+            const string expectedAnswer = "1 / 1\n";
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("1\n99999999 99999999 * 99999999 99999999")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass3()
+        {
+            // Arrange
+            const string expectedAnswer = "9999999800000001 / 9999999500000006\n";
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("1\n99999999 99999998 * 99999999 99999997")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass_When0()
+        {
+            // Arrange
+            const string expectedAnswer = "0 / 1\n";
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("1\n0 5 + 0 99")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass4()
+        {
+            // Arrange
+            const string expectedAnswer = "5 / 7\n";
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("1\n2 7 + 3 7")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass5()
+        {
+            // Arrange
+            const string expectedAnswer = "-1 / 7\n";
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("1\n2 7 - 3 7")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass6()
+        {
+            // Arrange
+            const string expectedAnswer = "6 / 49\n";
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("1\n2 7 * 3 7")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass7()
+        {
+            // Arrange
+            const string expectedAnswer = "2 / 3\n";
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("1\n2 7 / 3 7")))
             using (var output = new MemoryStream())
             {
                 // Act
